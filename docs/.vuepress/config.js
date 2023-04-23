15a9064f30cf6eac32c8c85faddf96e70959012e
Louis Holt <louis.holt@esprofiler.com>
2023-04-23 10:40:15 +0100

    Updated base url for gh-pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,7 @@ export default defineUserConfig({
   lang: 'en-US',
   title: 'Version 1.0',
   description: 'The first security framework focused on data. We\'re working to define what capabilities are needed to secure data in our world today.',
+  base: '/DSMM/',
   head: [
     ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
     ['meta', { name: 'theme-color', content: '#1bade4' }],
