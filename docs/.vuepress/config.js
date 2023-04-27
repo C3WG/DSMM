2b6e240dafd39fb76caa09c38ac35aef172f1e1c
Louis Holt <louis.holt@esprofiler.com>
2023-04-27 20:16:52 +0100

    Update version reference to v2.0

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ import { pwaPlugin } from '@vuepress/plugin-pwa'
 
 export default defineUserConfig({
   lang: 'en-US',
-  title: 'Version 1.0',
+  title: 'Version 2.0',
   description: 'The first security framework focused on data. We\'re working to define what capabilities are needed to secure data in our world today.',
   head: [
     ['meta', { name: 'theme-color', content: '#1bade4' }],
